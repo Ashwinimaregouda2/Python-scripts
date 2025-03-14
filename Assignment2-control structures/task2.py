9085fe63409d15700d4946049c5ad4dd96c75b99
Ashwinimaregouda2 <ashwini.maregouda@gmail.com>
2025-03-14 19:43:40 +0530

    Added files

---
+++ Assignment2-control structures/task2.py
@@ -0,0 +1,6 @@
+total_sum = 0
+
+for num in range(1, 51):  # Iterates from 1 to 50
+    total_sum += num       # Adds each number to the total sum
+
+print("The sum of numbers from 1 to 50 is:", total_sum)
